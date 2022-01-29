df263ca8aee44b2655c8dd006f418c2419e34530
snehafarkya <snhafarkya@gmail.com>
2022-01-29 23:57:38 +0530

    added links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,6 @@
 import React from "react";
 
 import Clock from "./Clock";
-import Apple from "./Apple";
-import Mango from "./Mango";
-import Watermelon from "./Watermelon";
-import Orange from "./Orange";
-import Banana from "./Banana";
-import Graps from "./Graps";
 
 import HeroImage from "./Rectangle.png";
 
@@ -23,46 +17,8 @@ export default function Home() {
       <div className="divv">
         <Clock />
       </div>
-      <h2>
-        {" "}
-        List of <span id="span">fruits</span>{" "}
-      </h2>
-
-      <h3>
-        <ul className="list">
-          <div className="div">
-            <li style={{ color: "#3D8952" }}>Apple</li>
-            <Origin originn="Europe" />
-
-            <Apple />
-          </div>
-          <div className="div">
-            <li style={{ color: "#3D8952" }}>Mango</li>
-            <Origin originn="India" />
-            <Mango />
-          </div>
-          <div className="div">
-            <li style={{ color: "#3D8952" }}>Watermelon</li>
-            <Origin originn="Africa" />
-            <Watermelon />
-          </div>
-          <div className="div">
-            <li style={{ color: "#3D8952" }}>Orange </li>
-            <Origin originn="Southeast Asia" />
-            <Orange />
-          </div>
-          <div className="div">
-            <li style={{ color: "#3D8952" }}>Banana</li>
-            <Origin originn="Southeast Asia" />
-            <Banana />
-          </div>
-          <div className="div">
-            <li style={{ color: "#3D8952" }}>Grapes </li>
-            <Origin originn="North America" />
-            <Graps />
-          </div>
-        </ul>
-      </h3>
+     
+      
     </>
   );
 }
